fba7f6d41b825ecf9b956cb89072a9d4e6fe8359
Ethan <elnair122@gmail.com>
2023-10-29 17:39:21 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
+// g++ -o run main.cpp
+// g++ -std=c++11 -o run main.cpp
+// ^ Fixes initializer list error
+int main()
+{
+    int n, x;
+    cout << "Hello world!" << endl;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    arr[0] = 50;
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        cout << arr[i] << endl;
+    }
+    x = 1;
+    int add;
+    while (x < 10)
+    {
+        cout << "Type a number to add to x: ";
+        cin >> add;
+        x += add;
+        cout << "X is: " << x << endl;
+    }
+    // maps
+    map<char, int> mp = {
+        {'T', 7},
+        {'S', 8},
+        {'a', 4}};
+    pair<char, int> p1('j', 5);
+    mp.insert(p1);
+    cout << mp['j'];
+    for (auto itr = mp.begin(); itr != mp.end(); ++itr)
+    {
+        cout << "Hi" << endl;
+    }
+
+    // vectors
+    vector<int> v1 = {1, 2, 3, 4};
+    v1.push_back(9);
+    v1.push_back(9);
+    v1.pop_back();
+    cout << v1.capacity() << endl;
+    cout << v1.size() << endl;
+    for (int i = 0; i < v1.size(); ++i)
+    {
+        cout << v1[i] << endl;
+    }
+    return 0;
+}
